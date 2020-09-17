a64a250f904f512ed1e397eab937decc10942da1
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-09-16 20:37:57 -0700

    create logo

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./logo.css";
+
+function Logo() {
+
+  return (
+    <img src={ require("../../images/logo.gif")} alt="Armonia" className="logo"/>
+  )
+
+}
+
+export default Logo;
